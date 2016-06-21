b5722f20d566245bbec0bfa8921096043ab56800
David Wolf <dmwguy@gmail.com>
2016-06-21 19:23:30 -0400

    hashing instead of plaintext passwords

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import bcrypt
 
 # This application shouldn't use multiple threads, but the debugger for Flask will, and therefore you'll want to disable
 # checking for multiple threads.
@@ -7,9 +8,10 @@ c = conn.cursor()
 
 
 def verify_password(u, p):
-    c.execute("SELECT pw FROM users WHERE user=?", (u,))
-    pw = c.fetchone()[0]
-    if p == pw:
+    c.execute("SELECT pwhash FROM users WHERE user=?", (u,))
+    pw_hash = c.fetchone()[0]
+    p_hash = bcrypt.hashpw(p.encode('UTF_8'), pw_hash)
+    if p_hash == pw_hash:
         return True
     else:
         return False
--- initialize.py
+++ initialize.py
@@ -1,21 +1,20 @@
 import sqlite3
+import bcrypt
 
 conn = sqlite3.connect("data.db")
 
 c = conn.cursor()
 
+pw = '123'
+pw_hash = bcrypt.hashpw('123'.encode('UTF_8'), bcrypt.gensalt())
+
 c.execute('''
 CREATE TABLE IF NOT EXISTS users (
   user TEXT,
-  pw TEXT
+  pwhash TEXT
 );''')
 
-c.execute('''
-INSERT INTO users
-  (user, pw)
-VALUES
-  ('dan', '123');
-''')
+c.execute('INSERT INTO users (user, pwhash) VALUES (?,?);', ('dan', pw_hash))
 
 conn.commit()
 conn.close()
